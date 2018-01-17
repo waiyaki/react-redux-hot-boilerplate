eb8148e66598000948c24e3a8939d3bf7b4780e0
Daniel Wangai <mainad57@gmail.com>
2018-01-17 22:37:15 +0300

    - add react-hot-loader - add devServer - enable HMR on development

--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,29 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
 
 import configureStore from './store';
 import App from './App';
 
 const store = configureStore();
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const renderApp = (Component) => {
+  render (
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+// render App first
+renderApp(App)
 
 if (process.env.NODE_ENV !== 'production' && module.hot) {
-  module.hot.accept();
+  module.hot.accept('./App.js', () => {
+    const HotApp = require('./App').default;
+    // render App on change
+    renderApp(HotApp);
+  });
 }
